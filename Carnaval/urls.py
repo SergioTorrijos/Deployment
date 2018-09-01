d4ba870c31ae52954c12e42b156b64194d18005d
SergioTorrijos <sergiotorrijos16@gmail.com>
2018-09-02 01:08:30 +0200

    first commit

--- Carnaval/urls.py
+++ Carnaval/urls.py
@@ -1,7 +1,7 @@
 #encoding:utf-8
 from django.conf.urls import *
 from django.contrib import admin
-from main.views import index, populateDBLetras, populateDBNoticias, entrenarDatos, resultadoEntrenarDatos
+from main.views import index, populateDBLetras, populateDBNoticias, entrenarDatos
 
 admin.autodiscover()
 
@@ -9,6 +9,5 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^populateLetras', populateDBLetras, name='populateDBLetras'),
     url(r'^populateNoticias',populateDBNoticias, name='populateDBNoticias'),
-    url(r'^entrenarDatos',entrenarDatos, name='entrenarDatos'),
-    url(r'^resultadoEntrenarDatos',resultadoEntrenarDatos, name='ResultadoEntrenarDatos'),]
+    url(r'^entrenarDatos',entrenarDatos, name='entrenarDatos')]
 
--- main/views.py
+++ main/views.py
@@ -27,20 +27,18 @@ def populateDBNoticias(request):
     numeroNoticias =str(cursor.fetchone()[0])
     return render_to_response('populateNoticias.html',{'numeroNoticias':numeroNoticias},context_instance=RequestContext(request))
 
-def resultadoEntrenarDatos(request):
-    conn = sqlite3.connect('carnaval.db')
-    cursor = conn.execute("SELECT distinct NOTICIA FROM ENTRENAMIENTO")
-    res=[]
-    for registro in cursor:
-        res.append(registro[0])
-        
-    return render_to_response('entreno.html', {'noticiasssss':res}, context_instance=RequestContext(request))
-
 def entrenarDatos(request):
     if request.method=='GET':
         form = UserForm(request.GET, request.FILES)
         if form.is_valid():
             guardarEntrenamiento(str(form.cleaned_data['id']))
+			conn = sqlite3.connect('carnaval.db')
+			cursor = conn.execute("SELECT distinct NOTICIA FROM ENTRENAMIENTO")
+			res=[]
+			for registro in cursor:
+				res.append(registro[0])
+				
+			return render_to_response('entreno.html', {'noticiasssss':res}, context_instance=RequestContext(request))
     else:
         form=UserForm()
     return render_to_response('entrenarDatos.html', {'form':form}, context_instance=RequestContext(request))
